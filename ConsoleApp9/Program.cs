ebe08bba6050fab938adf15cd970b07319acebcf
zelmanovic <zelmanovic@VT3-UCITEL>
2025-02-12 09:54:34 +0100

    úprava

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,17 +1,1 @@
-﻿Console.WriteLine("Uděl délku cesty (km): ");
-double delkaCesty = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Uděl spotřebu auta na 100km: ");
-double spotreba = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Kapacita nádrže: ");
-double kapacita = Convert.ToInt32(Console.ReadLine());
-
-double natankovat = Math.Floor(delkaCesty / spotreba / kapacita);
-Console.WriteLine($"Je potřeba zastavit a natankovat {natankovat}x.");
-
-double celkovaSpotreba = Math.Floor(delkaCesty / spotreba);
-Console.WriteLine($"Na celou cestu je potřeba {celkovaspotreba}L.");
-
-double palivoNaKonec = Math.Floor(celkovaSpotreba * natankovat / spotreba);
-Console.WriteLine($"Na konci cesty je potřeba {palivoNaKonec}L.");
+﻿//STEST
