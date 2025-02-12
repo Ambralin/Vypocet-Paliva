bd8f971602fc5240df0691a3452c0056f3b1e8be
zelmanovic <zelmanovic@VT3-UCITEL>
2025-02-12 10:04:23 +0100

    vrácení kódu

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,1 +1,17 @@
-﻿//STEST
+﻿Console.WriteLine("Uděl délku cesty (km): ");
+double delkaCesty = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Uděl spotřebu auta na 100km: ");
+double spotreba = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kapacita nádrže: ");
+double kapacita = Convert.ToInt32(Console.ReadLine());
+
+double natankovat = Math.Floor(delkaCesty / spotreba / kapacita);
+Console.WriteLine($"Je potřeba zastavit a natankovat {natankovat}x.");
+
+double celkovaspotreba = Math.Floor(delkaCesty / spotreba);
+Console.WriteLine($"Na celou cestu je potřeba {celkovaspotreba}L.");
+
+double palivonakonec = Math.Floor(celkovaspotreba * natankovat / spotreba);
+Console.WriteLine($"Na konci cesty je potřeba {palivonakonec}L.");
